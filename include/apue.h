727ab84f602f29d2a9bb811c56faefe3afebcb0e
leonshao <shao830417@163.com>
2014-12-08 23:45:07 +0800

    Add common header file.

---
+++ include/apue.h
@@ -0,0 +1,20 @@
+/*
+ * apue.h
+ *
+ *  Created on: 2014年12月8日
+ *      Author: leon
+ */
+
+#ifndef INCLUDE_APUE_H_
+#define INCLUDE_APUE_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define	MAXLINE 4096	/* max line length */
+
+void err_sys(const char *, ...);
+
+#endif /* INCLUDE_APUE_H_ */
---
+++ include/file_io.h
@@ -0,0 +1,15 @@
+/*
+ * file_io.h
+ *
+ *  Created on: 2014年12月8日
+ *      Author: leon
+ */
+
+#ifndef INCLUDE_FILE_IO_H_
+#define INCLUDE_FILE_IO_H_
+
+void set_fl(int fd, int flags);
+void clr_fl(int fd, int flags);
+
+
+#endif /* INCLUDE_FILE_IO_H_ */
